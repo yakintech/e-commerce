1cdc53e65ffd98e088b62f30e4a8b30159edee46
yakintech <cagatayyildiz87@gmail.com>
2024-12-10 14:42:58 +0300

    file upload sample

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app = express();
+const { BlobServiceClient } = require("@azure/storage-blob");
+const { v4: uuidv4 } = require("uuid");
+const cors = require('cors');
+const multer = require('multer');
+
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
@@ -25,7 +38,58 @@ app.get("/products", (req, res) => {
 });
 
 
- //
+
+//upload to azure blob POST request
+app.post("/upload",upload.array("files", 10), async (req, res) => {
+    try {
+        const file = req.files.file;
+        const folder = req.body.folder;
+        const blobName = await uploadToAzureBlob(folder, file);
+        if (blobName) {
+            return res.json({ success: true, blobName });
+        } else {
+            return res.json({ success: false });
+        }
+    } catch (error) {
+        console.log("Error uploading to Azure Blob", error);
+        return res.json({ success: false });
+    }
+});
+
+
+
+const AZURE_STORAGE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=neveracagatay;AccountKey=clZJ2G7LdrAbzt2C9vav7zZUBNweAmPvD8ZbdKLsPxeej3hQnOAyPzPL8kcfdjZaPIDQgANDvfeV+ASt8u8Blw==;EndpointSuffix=core.windows.net"
+
+async function uploadToAzureBlob(folder, file) {
+    const containerName = "izmir"
+    const accountName = "neveracagatay"
+
+    try {
+        
+
+        console.log("Uploading to Azure Blob",AZURE_STORAGE_CONNECTION_STRING);
+        const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
+        const containerClient = blobServiceClient.getContainerClient(containerName);
+
+        // Create the container if it does not exist
+        await containerClient.createIfNotExists();
+
+        // Blob name with folder
+        const blobName = `${folder}/${uuidv4()}-${file.originalname}`;
+        const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+        // Upload the file as a blob
+        await blockBlobClient.upload(file.buffer, file.buffer.length);
+        console.log(`https://${accountName}.blob.core.windows.net/${containerName}/${blobName}`);
+        return blobName;
+    } catch (error) {
+        console.log("Error uploading to Azure Blob", error);
+        return null;
+    }
+
+
+}
+
 
 
 app.listen(PORT, () => {
